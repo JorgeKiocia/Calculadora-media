9c600c1c7d3ba6c3ddd66fc248c88c695a4da2d0
JorgeKiocia <jorgekiocia@gmail.com>
2025-03-17 14:37:23 -0300

    Create calculator.py
    new python code

---
+++ calculator.py
@@ -0,0 +1,18 @@
+# Calculadora de Média de Notas do Jorge
+nota1 = float(input("Digite a primeira nota: ")) 
+nota2 = float(input("Digite a segunda nota: ")) 
+nota3 = float(input("Digite a terceira nota: ")) 
+ 
+
+media = (nota1 + nota2 + nota3) / 3 
+ 
+
+if media >= 7: 
+    status = "Aprovado" 
+elif media >= 5: 
+    status = "Recuperação" 
+else: 
+    status = "Reprovado" 
+ 
+
+print(f"Sua média é {media:.2f} - Status: {status}")
